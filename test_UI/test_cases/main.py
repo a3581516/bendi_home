741c4944d271798a5aa98e21c00b727573a23e2e
liujian <13850600551@163.com>
2019-06-30 22:37:41 +0800

    啦啦啦

--- test_UI/test_cases/main.py
+++ test_UI/test_cases/main.py
@@ -7,9 +7,10 @@ import os
 import sys
 aa=sys.path.append('./')
 print(aa)
-# #
+# #python test_UI\test_cases\main.py
 import pytest
 os.system("pytest -m smoke -s --html=reports.aaa.html --alluredir=/reports/allure_report")
-pytest.main(['-m', 'smoke'])
-pytest.main(['-m','invest'])
+# pytest.main(['-m', 'smoke'])
+# pytest.main(['-m','invest'])
+# pytest.main()
 
